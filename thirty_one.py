a58031180bcda97eb73775bb6f8bd804fa9d0bda
sh-0-go <shogo1216.kiki@gmail.com>
2023-07-28 15:18:29 +0900

    初期設定

--- thirty_one.py
+++ thirty_one.py
@@ -1,1 +1,41 @@
 import pygame
+
+pygame.init()
+
+# ウィンドウの作成
+screen_width = 600
+screen_height = 600
+screen = pygame.display.set_mode((screen_width, screen_height))
+pygame.display.set_caption("31 ゲーム")
+
+# 他の設定
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255) 
+
+# フォントの設定
+font = pygame.font.SysFont(None, 100)
+
+# メインループ#####################################################
+run = True
+
+while run:
+
+    # 背景の塗りつぶし
+    screen.fill(WHITE)
+    
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_ESCAPE:
+                run = False
+
+    # 更新
+    pygame.display.update()
+
+##################################################################
+
+pygame.quit()
